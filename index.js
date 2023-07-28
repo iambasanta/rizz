d954c9989e8834df35a4e6c5b64069ccbe776075
iambasanta <iambasantarai@gmail.com>
2023-07-28 21:08:40 +0545

    Fetch data from api

--- index.js
+++ index.js
@@ -3,9 +3,26 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const axios = require("axios");
 
 const PORT = process.env.PORT || 8000;
 app.use(express.json());
+
+const rizzMe = async () => {
+  const options = {
+    method: "GET",
+    url: process.env.API_URL,
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log(response.data.pickup);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+rizzMe()
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);

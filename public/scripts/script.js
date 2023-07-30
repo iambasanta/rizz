6a379397fa6e0a8daa1f205883efb8ef27951c1f
iambasanta <iambasantarai@gmail.com>
2023-07-30 09:13:54 +0545

    Implement copy to clipboard

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,28 @@
 "use strict";
 
 const generateButton = document.getElementById("generateButton");
+const copyToClipboardButton = document.getElementById("copyToClipboardButton");
+const rizzContent = document.getElementById("rizzContent");
+const copiedMessage = document.getElementById("copiedMessage");
 
 generateButton.addEventListener("click", () => {
   location.reload();
 });
+
+copyToClipboardButton.addEventListener("click", () => {
+  navigator.clipboard
+    .writeText(rizzContent.textContent)
+    .then(() => {
+      showCopiedMessage();
+    })
+    .catch((error) => {
+      console.error("Failed to copy to clipboard:", error);
+    });
+});
+
+function showCopiedMessage() {
+  copiedMessage.style.display = "block";
+  setTimeout(() => {
+    copiedMessage.style.display = "none";
+  }, 1000);
+}
